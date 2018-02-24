0a34fc22d0cd3479f966df25458ffb008fed8365
Leandro dos Santos <leandrosantossjc@gmail.com>
2018-02-24 11:03:14 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1519445690,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/card-sheet0.png",
+		"images/card-sheet1.png",
+		"media/142608__autistic-lucario__error.m4a",
+		"media/142608__autistic-lucario__error.ogg",
+		"media/30232__lg__hits37.m4a",
+		"media/30232__lg__hits37.ogg",
+		"media/242501__gabrielaraujo__powerup-success.m4a",
+		"media/242501__gabrielaraujo__powerup-success.ogg",
+		"media/carefree.m4a",
+		"media/carefree.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
